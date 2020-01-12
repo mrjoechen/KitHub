b8545bcb290507f04a9a5a73711539bb4e535361
chenqiao <mrjoechen@qq.com>
2020-01-13 00:05:49 +0800

    PreferenceExt

---
+++ app/src/main/java/com/chenqiao/kithub/App.kt
@@ -0,0 +1,17 @@
+package com.chenqiao.kithub
+
+import android.app.Application
+
+/**
+ * Created by chenqiao on 2020-01-13.
+ * e-mail : mrjctech@gmail.com
+ */
+
+class App : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
