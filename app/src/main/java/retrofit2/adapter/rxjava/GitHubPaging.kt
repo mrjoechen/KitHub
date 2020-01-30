751b9bae0c8ad65e3fb060c2f5e59e564305f8d7
chenqiao <mrjoechen@qq.com>
2020-01-30 23:56:54 +0800

    Login

--- app/src/main/java/retrofit2/adapter/rxjava/GitHubPaging.kt
+++ app/src/main/java/retrofit2/adapter/rxjava/GitHubPaging.kt
@@ -3,12 +3,13 @@ package retrofit2.adapter.rxjava
 import com.chenqiao.common.ext.logger
 import okhttp3.HttpUrl
 
-class GitHubPaging<T>: ArrayList<T>() {
+class GitHubPaging<T> : ArrayList<T>() {
     companion object {
-        const val URL_PATTERN = """(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"""
+        const val URL_PATTERN =
+            """(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"""
     }
 
-    private  val relMap = HashMap<String, String?>().withDefault { null }
+    private val relMap = HashMap<String, String?>().withDefault { null }
 
     private val first by relMap
     private val last by relMap
@@ -29,27 +30,26 @@ class GitHubPaging<T>: ArrayList<T>() {
 
     var since: Int = 0
 
-    operator fun get(key: String): String?{
+    operator fun get(key: String): String? {
         return relMap[key]
     }
 
-    fun setupLinks(link: String){
+    fun setupLinks(link: String) {
         logger.warn("setupLinks: $link")
         Regex("""<($URL_PATTERN)>; rel="(\w+)"""").findAll(link).asIterable()
-                .map {
-                    matchResult ->
-                    val url = matchResult.groupValues[1]
-                    relMap[matchResult.groupValues[3]] = url // next=....
-                    if(url.contains("since")){
-                        HttpUrl.parse(url)?.queryParameter("since")?.let{
-                            since = it.toInt()
-                        }
+            .map { matchResult ->
+                val url = matchResult.groupValues[1]
+                relMap[matchResult.groupValues[3]] = url // next=....
+                if (url.contains("since")) {
+                    HttpUrl.parse(url)?.queryParameter("since")?.let {
+                        since = it.toInt()
                     }
-                    logger.warn("${matchResult.groupValues[3]} => ${matchResult.groupValues[1]}")
                 }
+                logger.warn("${matchResult.groupValues[3]} => ${matchResult.groupValues[1]}")
+            }
     }
 
-    fun mergeData(paging: GitHubPaging<T>): GitHubPaging<T>{
+    fun mergeData(paging: GitHubPaging<T>): GitHubPaging<T> {
         addAll(paging)
         relMap.clear()
         relMap.putAll(paging.relMap)
--- app/src/main/java/retrofit2/adapter/rxjava/RxJavaCallAdapter2.java
+++ app/src/main/java/retrofit2/adapter/rxjava/RxJavaCallAdapter2.java
@@ -27,66 +27,68 @@ import rx.Observable.OnSubscribe;
 import rx.Scheduler;
 
 final class RxJavaCallAdapter2<R> implements CallAdapter<R, Object> {
-  private final Type responseType;
-  private final @Nullable Scheduler schedulerSubscribeOn;
-  private final @Nullable Scheduler schedulerObserveOn;
-  private final boolean isAsync;
-  private final boolean isResult;
-  private final boolean isBody;
-  private final boolean isPaging;
-  private final boolean isSingle;
-  private final boolean isCompletable;
+    private final Type responseType;
+    private final @Nullable
+    Scheduler schedulerSubscribeOn;
+    private final @Nullable
+    Scheduler schedulerObserveOn;
+    private final boolean isAsync;
+    private final boolean isResult;
+    private final boolean isBody;
+    private final boolean isPaging;
+    private final boolean isSingle;
+    private final boolean isCompletable;
 
-  RxJavaCallAdapter2(Type responseType, @Nullable Scheduler schedulerSubscribeOn, @Nullable Scheduler schedulerObserveOn, boolean isAsync,
-                     boolean isResult, boolean isBody, boolean isPaging, boolean isSingle, boolean isCompletable) {
-    this.responseType = responseType;
-    this.schedulerSubscribeOn = schedulerSubscribeOn;
-    this.schedulerObserveOn = schedulerObserveOn;
-    this.isAsync = isAsync;
-    this.isResult = isResult;
-    this.isBody = isBody;
-    this.isPaging = isPaging;
-    this.isSingle = isSingle;
-    this.isCompletable = isCompletable;
-  }
-
-  @Override
-  public Type responseType() {
-    return responseType;
-  }
-
-  @Override
-  public Object adapt(Call<R> call) {
-    OnSubscribe<Response<R>> callFunc = isAsync
-        ? new CallEnqueueOnSubscribe<>(call)
-        : new CallExecuteOnSubscribe<>(call);
-
-    OnSubscribe<?> func;
-    if (isResult) {
-      func = new ResultOnSubscribe<>(callFunc);
-    } else if (isBody) {
-      func = new BodyOnSubscribe<>(callFunc);
-    } else if(isPaging) {
-      func = new GitHubListOnSubscribe<>(callFunc);
-    } else {
-        func = callFunc;
-    }
-    Observable<?> observable = Observable.create(func);
-
-    if (schedulerSubscribeOn != null) {
-      observable = observable.subscribeOn(schedulerSubscribeOn);
+    RxJavaCallAdapter2(Type responseType, @Nullable Scheduler schedulerSubscribeOn, @Nullable Scheduler schedulerObserveOn, boolean isAsync,
+                       boolean isResult, boolean isBody, boolean isPaging, boolean isSingle, boolean isCompletable) {
+        this.responseType = responseType;
+        this.schedulerSubscribeOn = schedulerSubscribeOn;
+        this.schedulerObserveOn = schedulerObserveOn;
+        this.isAsync = isAsync;
+        this.isResult = isResult;
+        this.isBody = isBody;
+        this.isPaging = isPaging;
+        this.isSingle = isSingle;
+        this.isCompletable = isCompletable;
     }
 
-    if (schedulerObserveOn != null) {
-      observable = observable.observeOn(schedulerObserveOn);
+    @Override
+    public Type responseType() {
+        return responseType;
     }
 
-    if (isSingle) {
-      return observable.toSingle();
+    @Override
+    public Object adapt(Call<R> call) {
+        OnSubscribe<Response<R>> callFunc = isAsync
+                ? new CallEnqueueOnSubscribe<>(call)
+                : new CallExecuteOnSubscribe<>(call);
+
+        OnSubscribe<?> func;
+        if (isResult) {
+            func = new ResultOnSubscribe<>(callFunc);
+        } else if (isBody) {
+            func = new BodyOnSubscribe<>(callFunc);
+        } else if (isPaging) {
+            func = new GitHubListOnSubscribe<>(callFunc);
+        } else {
+            func = callFunc;
+        }
+        Observable<?> observable = Observable.create(func);
+
+        if (schedulerSubscribeOn != null) {
+            observable = observable.subscribeOn(schedulerSubscribeOn);
+        }
+
+        if (schedulerObserveOn != null) {
+            observable = observable.observeOn(schedulerObserveOn);
+        }
+
+        if (isSingle) {
+            return observable.toSingle();
+        }
+        if (isCompletable) {
+            return observable.toCompletable();
+        }
+        return observable;
     }
-    if (isCompletable) {
-      return observable.toCompletable();
-    }
-    return observable;
-  }
 }
